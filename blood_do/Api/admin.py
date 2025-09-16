88c61df320f7b7d0a0ae8f811eace0d6fb0074a2
Jescar <jescar@gmail.com>
2025-09-16 15:46:22 +0300

    Hello

--- blood_do/Api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Admin, Donor, Donation, Recipient, BloodRequest
-
-# Register your models here.
-admin.site.register(Admin)
-admin.site.register(Donor)
-admin.site.register(Donation)
-admin.site.register(Recipient)
-admin.site.register(BloodRequest)
--- blood_do/Api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BloodDonnectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Api'
